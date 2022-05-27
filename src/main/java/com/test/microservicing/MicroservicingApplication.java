2780aa4ac28ec8ed2fe2dc43838b8fcc9967536e
john <johnrexcyvelez@gmail.com>
2022-05-28 01:22:20 +0800

    first commit

---
+++ src/main/java/com/test/microservicing/MicroservicingApplication.java
@@ -0,0 +1,13 @@
+package com.test.microservicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/microservicing/MicroservicingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.microservicing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
